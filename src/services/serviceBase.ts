16e5dfbcd9a31ba730e532c8762dfe70fff50eb6
Camilla Ridesjö <camilla.ridesjo@gmail.com>
2023-09-15 11:47:48 +0200

    Added file for API calls

---
+++ src/services/serviceBase.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://jobed-connect-api.jobtechdev.se/';
+
+export const get = async <T>(url: string): Promise<T> => {
+    const response = await axios.get<T>(`${BASE_URL}${url}`);
+    return response.data;
+};
+
+export const post = async <T>(url: string, data: T): Promise<T> => {
+    const response = await axios.post<T>(`${BASE_URL}${url}`, data);
+    return response.data;
+};
+
+export const matchByText = async (text: string) => {
+  return post('/v1/occupations/match-by-text', { text });
+};
+
+export const getEnrichedOccupations = async (occupationId: string) => {
+  return get(`/v1/enriched_occupations/${occupationId}`);
+};
