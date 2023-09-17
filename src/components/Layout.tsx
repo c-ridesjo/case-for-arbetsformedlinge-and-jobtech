bffb13fa9e56f65006255b23e5bcd033531296c0
Malin Dahlquist <m-dahlquist@hotmail.com>
2023-09-17 21:15:38 +0200

    add html to searchform

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,20 +1,22 @@
-import { Home } from "./Home"
-import { DigiTypographyMeta } from '@digi/arbetsformedlingen-react'
+import { Home } from "./Home";
+import { DigiTypographyMeta } from "@digi/arbetsformedlingen-react";
 
 export const Layout = () => {
-    return (
-        <>
-        <DigiTypographyMeta>
-            <header><h1>Logga</h1></header>
-          
-          <main>
-            <Home></Home>
-          </main>
-  
-           <footer><h3>Footertext</h3></footer> 
-        </DigiTypographyMeta>
-          
+  return (
+    <>
+      <DigiTypographyMeta>
+        <header>
+          <h1>Logga</h1>
+        </header>
 
-        </>
-    )
-}
+        <main>
+          <Home></Home>
+        </main>
+
+        <footer>
+          <h3>Footertext</h3>
+        </footer>
+      </DigiTypographyMeta>
+    </>
+  );
+};
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,5 +1,23 @@
+import { DigiTypographyMeta } from "@digi/arbetsformedlingen-react";
+
 export const SearchForm = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <DigiTypographyMeta>
+      <header>
+        <h1>Logga</h1>
+      </header>
+
+      <main>
+        <form>
+          <input type="text" placeholder="Utbildningstitel"></input>
+          <input type="text" placeholder="Beskrivning"></input>
+          <button type="submit">Sök yrken</button>
+        </form>
+      </main>
+
+      <footer>
+        <h3>Footertext</h3>
+      </footer>
+    </DigiTypographyMeta>
+  );
+};
