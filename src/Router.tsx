47fb852e495ad7b8b20c6cb94d8eb83ce9b27871
Malin Dahlquist <m-dahlquist@hotmail.com>
2023-09-17 21:15:38 +0200

    add html to searchform

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,27 +1,32 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { Home } from './components/Home';
-import { Layout } from './components/Layout';
-import { SelectedJob } from './components/SelectedJob';
-import { SearchResults } from './components/SearchResults';
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "./components/Home";
+import { Layout } from "./components/Layout";
+import { SelectedJob } from "./components/SelectedJob";
+import { SearchResults } from "./components/SearchResults";
+import { SearchForm } from "./components/SearchForm";
 
 export const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Layout />,
-		children: [
-			{
-				path: '/',
-				element: <Home />,
-				index: true,
-			},
-			{
-				path: '/search-results',
-				element: <SearchResults />,
-			},
-			{
-				path: '/selected-job',
-				element: <SelectedJob />,
-			},
-		],
-	},
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        index: true,
+      },
+      {
+        path: "/search-form",
+		element: <SearchForm />,
+      },
+      {
+        path: "/search-results",
+        element: <SearchResults />,
+      },
+      {
+        path: "/selected-job",
+        element: <SelectedJob />,
+      },
+    ],
+  },
 ]);
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,20 +1,22 @@
-import { DigiTypographyMeta } from '@digi/arbetsformedlingen-react'
-import { Outlet } from "react-router-dom"
+import { DigiTypographyMeta } from "@digi/arbetsformedlingen-react";
+import { Outlet } from "react-router-dom";
 
 export const Layout = () => {
-    return (
-        <>
-        <DigiTypographyMeta>
-            <header><h1>Logga</h1></header>
-          
-          <main>
-            <Outlet></Outlet>
-          </main>
-  
-           <footer><h3>Footertext</h3></footer> 
-        </DigiTypographyMeta>
-          
+  return (
+    <>
+      <DigiTypographyMeta>
+        <header>
+          <h1>Logga</h1>
+        </header>
 
-        </>
-    )
-}
+        <main>
+          <Outlet></Outlet>
+        </main>
+
+        <footer>
+          <h3>Footertext</h3>
+        </footer>
+      </DigiTypographyMeta>
+    </>
+  );
+};
