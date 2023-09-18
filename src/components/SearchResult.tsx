330725df91ac61f3cc6b8aca1ab469762c556450
Camilla Ridesjö <camilla.ridesjo@gmail.com>
2023-09-18 15:50:15 +0200

    Added search function

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,5 +1,13 @@
-export const SearchResult = () => {
-    return (
-        <></>
-    )
-}
+import { useNavigate } from 'react-router-dom';
+import { SearchResultProps } from '../models/ISearchResult';
+
+export const SearchResult = ({ title, description, link }: SearchResultProps) => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <button onClick={() => navigate(link)}>Read More</button>
+    </>
+  );
+};
