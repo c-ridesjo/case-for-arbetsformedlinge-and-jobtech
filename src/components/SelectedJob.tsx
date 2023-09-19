2d80b2382a774183edc35684e9769a8959a85c44
Malin Dahlquist <m-dahlquist@hotmail.com>
2023-09-19 21:03:37 +0200

    make get request to enriched occupations endpoint

--- src/components/SelectedJob.tsx
+++ src/components/SelectedJob.tsx
@@ -1,8 +1,17 @@
+import axios from "axios";
 import { useParams } from "react-router-dom";
 
 export const SelectedJob = () => {
   const { occupationId } = useParams();
   console.log(occupationId);
+
+  axios
+    .get(
+      `https://jobed-connect-api.jobtechdev.se/v1/enriched_occupations?occupation_id=${occupationId}`
+    )
+    .then((res) => {
+      console.log(res);
+    });
 
   return <></>;
 };
