7d7a9baae51e5f2d08c8d9bd37ca68c38a108100
Camilla Ridesjö <camilla.ridesjo@gmail.com>
2023-09-19 15:33:38 +0200

    Troubleshooting 404 error

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -19,28 +19,40 @@ export const SearchResults = () => {
   const navigate = useNavigate();
   const [error, setError] = useState<string | null>(null);
 
-  const fetchOccupationsByText = async (text: string) => {
+  const fetchOccupationsByText = async () => {
+    // (text: string) sedan när jag inte hårdkodar
+    setError(null);
     try {
-        const response = await axios.post('https://jobed-connect-api.jobtechdev.se/v1/occupations/match-by-text', {
-        input_text: text, 
-        input_headline: "front end developer", 
-        limit: 10,
-        offset: 0,
-        include_metadata: false
-      });      
-      
-      setData(response.data.related_occupations);   
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      setError('Ett fel uppstod vid hämtning av data. Vänligen försök igen senare.');
+      const response = await axios.post(
+        "https://jobed-connect-api.jobtechdev.se/v1/occupations/match-by-text",
+        {
+          input_text: "front end developer",
+          input_headline: "systemutvecklare",
+          limit: 10,
+          offset: 0,
+          include_metadata: false,
+        }
+      );
+
+      setData(response.data.related_occupations);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error("Error fetching data:", error.message);
+      } else {
+        console.error("An unknown error occurred:", error);
+      }
+      setError(
+        "Ett fel uppstod vid hämtning av data. Vänligen försök igen senare."
+      );
     }
   };
-  
-  
 
   useEffect(() => {
-    fetchOccupationsByText(educationTitle);
+    console.log('educationTitle value:', educationTitle);
+    fetchOccupationsByText();
   }, [educationTitle]);
+  
+  
 
   return (
     <>
@@ -49,17 +61,17 @@ export const SearchResults = () => {
           <DigiButton onClick={() => navigate("/")}>Home</DigiButton>
           <h2>Titel</h2>
           <p>Beskrivning {description}</p>
-          {error && <p style={{ color: 'red' }}>{error}</p>}
+          {error && <p style={{ color: "red" }}>{error}</p>}
           <div className="box-container">
-        {data.map((occupation: OccupationData) => (
-          <SearchResult
-            key={occupation.id}
-            title={occupation.occupation_label} 
-            description={occupation.description} // ?
-            link={`/selected-job/${occupation.id}`}
-          />
-        ))}
-      </div>
+            {data.map((occupation: OccupationData) => (
+              <SearchResult
+                key={occupation.id}
+                title={occupation.occupation_label}
+                description={occupation.description} // ?
+                link={`/selected-job/${occupation.id}`}
+              />
+            ))}
+          </div>
         </DigiLayoutContainer>
       </DigiTypographyMeta>
     </>
