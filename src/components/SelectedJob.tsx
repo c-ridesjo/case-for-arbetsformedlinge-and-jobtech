402a24885a84b85a8a0038ed2c0ee5d6fe3f98ca
Malin Dahlquist <m-dahlquist@hotmail.com>
2023-09-19 21:26:22 +0200

    add interface and prevent infinite loop

--- src/components/SelectedJob.tsx
+++ src/components/SelectedJob.tsx
@@ -2,19 +2,34 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 
+interface OccupationDetails {
+  id: string;
+  occupation_label: string;
+  concept_taxonomy_id: string;
+  legacy_ams_taxonomy_id: string;
+  occupation_group: {
+    occupation_group_label: string;
+    concept_taxonomy_id: string;
+    ssyk: string;
+  };
+}
+
 export const SelectedJob = () => {
   const { occupationId } = useParams();
   console.log(occupationId);
-  const [occupationDetails, setOccupationDetails] = useState({} as any);
+  const [occupationDetails, setOccupationDetails] = useState<
+    OccupationDetails | undefined
+  >(undefined);
 
-  axios
-    .get(
-      `https://jobed-connect-api.jobtechdev.se/v1/enriched_occupations?occupation_id=${occupationId}`
-    )
-    .then((res) => {
-      console.log(res);
-      setOccupationDetails(res.data);
-    });
-
+  if (occupationDetails === undefined) {
+    axios
+      .get(
+        `https://jobed-connect-api.jobtechdev.se/v1/enriched_occupations?occupation_id=${occupationId}`
+      )
+      .then((res) => {
+        console.log(res);
+        setOccupationDetails(res.data);
+      });
+  }
   return <>{JSON.stringify(occupationDetails, null, 4)}</>;
 };
