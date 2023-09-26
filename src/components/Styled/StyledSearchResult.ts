e05a326374edae17dc0935f6115a217b371b146d
EZillman <evelina.zillman@medieinstitutet.se>
2023-09-26 18:55:57 +0200

    [added] media query to styled components

--- src/components/Styled/StyledSearchResult.ts
+++ src/components/Styled/StyledSearchResult.ts
@@ -3,18 +3,27 @@ import styled from "styled-components";
 export const ColumnContainer = styled.div`
   display: flex;
   flex-direction: column;
-  //flex-wrap: wrap; använd för desktop 
   justify-content: center;
   align-items: center;
-  //margin-top: 2rem;
   margin: auto;
   max-width: 17rem;
+
+  @media screen and (min-width: 900px) {
+    flex-wrap: wrap;
+    flex-direction: row;
+    max-width: 85%;
+  }
 `;
 
 export const Column = styled.div`
   margin: 0 0.5rem;
   max-width: 20rem;
   overflow: hidden;
+
+  @media screen and (min-width: 900px) {
+    overflow: visible;
+    max-width: 90%;
+  }
 `;
 
 export const VerticalLayout = styled.div`
