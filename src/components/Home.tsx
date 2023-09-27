d8d2e91dea11af961af54ba568beae005465b7c4
EZillman <evelina.zillman@medieinstitutet.se>
2023-09-27 11:11:30 +0200

    [fixed] empty input change

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
 import { DigiTypographyMeta } from "@digi/arbetsformedlingen-react";
-import { SearchForm } from "./SearchForm";
+import { IFormData, SearchForm } from "./SearchForm";
 import { SearchResults } from "./SearchResults";
 import { matchByText } from "../services/serviceBase";
 import { useState } from "react";
@@ -7,10 +7,7 @@ import { useState } from "react";
 export const Home = () => {
   const [responseData, setResponseData] = useState({});
 
-  const handleFormSubmit = async (formData: {
-    educationTitle: string;
-    description: string;
-  }) => {
+  const handleFormSubmit = async (formData: IFormData) => {
     console.log("data", formData);
     try {
       const dataFromResponse = await matchByText(
